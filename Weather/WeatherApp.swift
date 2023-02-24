ef69c87cb4c4a765519b3e20ab86b7ca1e828d69
Abdullah Hashim <abdullah@drahim.sa>
2023-02-24 19:15:18 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Abdullah Hashim on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

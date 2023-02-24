dcfe6abdb0a7f239fa6063602c7ee3e4eb69b377
Abdullah Hashim <abdullah@drahim.sa>
2023-02-25 00:51:59 +0300

    initial commit

---
+++ Weather/Utils/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by Abdullah Hashim on 24/02/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = LinearGradient(
+        gradient: Gradient, startPoint: <#T##UnitPoint#>, endPoint: <#T##UnitPoint#>
+    )
+}
---
+++ Weather/Views/Details/ForcaseView.swift
@@ -0,0 +1,52 @@
+//
+//  ForcaseView.swift
+//  Weather
+//
+//  Created by Abdullah Hashim on 24/02/2023.
+//
+
+import SwiftUI
+
+struct ForcaseView: View {
+    var bottomSheetTranslationProrated: CGFloat = 1
+    var body: some View {
+        ScrollView {
+            
+        }
+        .backgroundBlur(raduis: 25,opaque: true)
+        .background(Color.bottomSheetBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 44))
+        .innerShadow(
+            shape: RoundedRectangle(cornerRadius: 44),
+            color: Color.bottomSheetBorderMiddle,
+            linewidth: 1,
+            offsetX: 0,
+            offsetY: 1,
+            blur: 0,
+            blendMode: .overlay,
+            opacity: 1 - bottomSheetTranslationProrated
+        )
+        .overlay {
+            Divider()
+                .background(Color.bottomSheetBorderTop)
+                .frame(maxHeight: .infinity,alignment: .top)
+                .clipShape(RoundedRectangle(cornerRadius: 44))
+        }
+        .overlay {
+            // MARK: Drag indicator
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.black.opacity(0.3))
+                .frame(width: 48,height: 5)
+                .frame(height: 20)
+                .frame(maxHeight: .infinity,alignment: .top)
+        }
+    }
+}
+
+struct ForcaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForcaseView()
+            .background(Color.background)
+            .preferredColorScheme(.dark)
+    }
+}
